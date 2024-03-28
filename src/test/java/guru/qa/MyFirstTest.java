3364c45e3092d460da5a6761df783558f1bf8fb6
NovruzSalakhov <salakhov-novruz@mail,ru>
2024-03-28 17:24:17 +0300

    added gitignore and MyFirstTest class

---
+++ src/test/java/guru/qa/MyFirstTest.java
@@ -0,0 +1,25 @@
+package guru.qa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class MyFirstTest {
+
+    @BeforeAll
+    static void beforeAll (){
+        Configuration.browser="chrome";
+    }
+
+    @Test
+    void firstTest () {
+        open("https://google.com");
+
+    }
+
+
+
+}
+
